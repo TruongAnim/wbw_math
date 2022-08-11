357142ed473665fb086686ba06520ab333cc6f0e
TruongAnim <truonganim.dev@gmail.com>
2022-08-11 22:08:09 +0700

    edit area of common shape

--- projects/perimeter_and_area/area_of_common_shape/main_scene.py
+++ projects/perimeter_and_area/area_of_common_shape/main_scene.py
@@ -1,10 +1,9 @@
-import numpy as np
 from manim import *
 
-list_scene = ("Scene1", "Scene2", "Scene3", "Scene4", "Scene5", "Scene6")
-SCENE_NAME = list_scene[4]
+list_scene = ("Scene1", "Scene2", "Scene3", "Scene4", "Scene5")
+SCENE_NAME = " ".join(list_scene)
 CONFIG_DIR = "../../../configs/"
-CONFIG = "develop.cfg"
+CONFIG = "production.cfg"
 
 if __name__ == "__main__":
     command = f"manim --disable_caching -c {CONFIG_DIR}{CONFIG} {__file__} {SCENE_NAME}"
@@ -23,23 +22,39 @@ class ProofScene(Scene):
             .next_to(self.title_en, DOWN)
 
     def draw_title(self, other_obj):
-        self.play(Write(self.title_en), Write(self.title_vn),
-                  Write(other_obj), )
+        self.my_play(Write(self.title_en), Write(self.title_vn),
+                     Write(other_obj), )
 
     def creat_formula(self, formula):
         self.formula = MathTex(formula).scale(1.5).shift(DOWN * 2.5)
 
     def draw_formula(self, indexs):
-        self.play(LaggedStart(*[
+        self.my_play(LaggedStart(*[
             DrawBorderThenFill(self.formula[0][i]) for i in indexs
         ], lag_ratio=0.1))
-        self.play(Circumscribe(self.formula))
+        self.my_play(Circumscribe(self.formula, run_time=2))
+
+    def my_play(
+            self,
+            *args,
+            subcaption=None,
+            subcaption_duration=None,
+            subcaption_offset=0,
+            **kwargs,
+    ):
+        if "run_time" not in kwargs:
+            kwargs["run_time"] = 2
+        super().play(*args,
+                     subcaption=subcaption,
+                     subcaption_duration=subcaption_duration,
+                     subcaption_offset=subcaption_offset,
+                     **kwargs)
+        self.wait()
 
 
 # acute triangle
 class Scene1(ProofScene):
     def setup(self):
-        import numpy as np
         self.create_title(r"Acute triangle", r"(Tam giác nhọn)")
 
     def construct(self):
@@ -62,32 +77,36 @@ class Scene1(ProofScene):
         sub_left_2 = VMobject(fill_color=YELLOW, **kwargs).set_points_as_corners([E, K, H, B, E])
         sub_right_1 = VMobject(fill_color=RED, **kwargs).set_points_as_corners([A, K, F, A])
         sub_right_2 = VMobject(fill_color=RED, **kwargs).set_points_as_corners([F, K, H, C, F])
-        rectangle = Rectangle(width=4, height=1.5).align_to(main_tri, DL)
+        rectangle = Rectangle(width=4, height=1.5, color=GREEN).align_to(main_tri, DL)
 
         brace_h = Brace(main_tri, LEFT)
-        text_h = brace_h.get_text("h")
+        text_h = brace_h.get_text("$$h$$")
         brace_b = Brace(main_tri, DOWN)
-        text_b = brace_b.get_text("b")
+        text_b = brace_b.get_text("$$b$$")
         brace_rec = Brace(rectangle, RIGHT)
         text_rec = brace_rec.get_text(r"$${h}\over{2}$$")
 
         self.draw_title(main_tri)
 
-        self.play(*[
-            Write(i) for i in (brace_b, brace_h, text_b, text_h)
+        self.my_play(*[
+            FadeIn(i, shift=j) for i, j in zip(
+                (brace_b, brace_h, text_b, text_h),
+                (UP, RIGHT, UP, RIGHT)
+            )
         ])
-        self.play(*[
+        self.my_play(*[
             FadeIn(i) for i in (sub_left, sub_right)
         ], Create(line))
         self.remove(sub_left, sub_right)
         self.add(sub_left_1, sub_left_2, sub_right_1, sub_right_2)
-        self.play(Create(rectangle), Write(brace_rec), Write(text_rec))
-        self.play(Rotate(sub_left_1, PI, about_point=E),
-                  Rotate(sub_right_1, -PI, about_point=F))
+        self.my_play(Create(rectangle), FadeIn(brace_rec, shift=LEFT),
+                     FadeIn(text_rec, shift=LEFT))
+        self.my_play(Rotate(sub_left_1, PI, about_point=E),
+                     Rotate(sub_right_1, -PI, about_point=F))
 
         self.creat_formula(r"S = {{h \times b} \over 2}")
-        self.play(FadeTransform(text_h.copy(), self.formula[0][2]),
-                  FadeTransform(text_b.copy(), self.formula[0][4]))
+        self.my_play(FadeTransform(text_h.copy(), self.formula[0][2]),
+                     FadeTransform(text_b.copy(), self.formula[0][4]))
         self.draw_formula((0, 1, 3, 5, 6))
 
 
@@ -116,29 +135,33 @@ class Scene2(ProofScene):
         sub_line = Line(start=H, end=K, stroke_width=1)
 
         brace_h = Brace(main_para, LEFT)
-        text_h = brace_h.get_text("h")
+        text_h = brace_h.get_text("$$h$$")
         brace_b = Brace(Line(start=B, end=C), DOWN)
-        text_b = brace_b.get_text("b")
+        text_b = brace_b.get_text("$$b$$")
         h = MathTex("h").next_to(line, RIGHT)
 
         right_angle = RightAngle(line, sub_line, quadrant=(-1, 1), stroke_width=2)
+        rectangle = Rectangle(width=4, height=3, color=GREEN).align_to(main_para, UR)
 
         self.draw_title(main_para)
 
-        self.play(*[
-            Write(i) for i in (brace_b, brace_h, text_b, text_h)
+        self.my_play(*[
+            FadeIn(i, shift=j) for i, j in zip(
+                (brace_b, brace_h, text_b, text_h),
+                (UP, RIGHT, UP, RIGHT)
+            )
         ])
-        self.play(*[
+        self.my_play(*[
             FadeIn(i) for i in (ABHA, AHCDA)
-        ], Create(line), Write(h), Create(right_angle))
+        ], Create(line), ReplacementTransform(text_h.copy(), h), Create(right_angle))
 
-        self.play(*[
+        self.my_play(*[
             i.animate.shift(RIGHT * 1.5) for i in (brace_b, text_b)
-        ], ABHA.animate.shift(RIGHT * 4), run_time=3, rate_func=linear)
-
+        ], ABHA.animate.shift(RIGHT * 4.01), run_time=3, rate_func=linear)
+        self.my_play(Create(rectangle))
         self.creat_formula(r"S = {h \times b}")
-        self.play(FadeTransform(text_h.copy(), self.formula[0][2]),
-                  FadeTransform(text_b.copy(), self.formula[0][4]))
+        self.my_play(FadeTransform(text_h.copy(), self.formula[0][2]),
+                     FadeTransform(text_b.copy(), self.formula[0][4]))
         self.draw_formula((0, 1, 3))
 
 
@@ -166,39 +189,42 @@ class Scene3(ProofScene):
         FCD = VMobject(fill_color=RED, **kwargs).set_points_as_corners([F, C, D, F])
         KDA = VMobject(fill_color=BLUE, **kwargs).set_points_as_corners([K, D, A, K])
 
-        rectangle = Rectangle(width=3, height=1.9).align_to(main_tri, DL)
+        rectangle = Rectangle(width=3, height=1.9, color=GREEN).align_to(main_tri, DL)
 
         brace_h = Brace(main_tri, RIGHT)
-        text_h = brace_h.get_text("h")
+        text_h = brace_h.get_text("$$h$$")
         brace_b = Brace(rectangle, DOWN)
-        text_b = brace_b.get_text("b")
+        text_b = brace_b.get_text("$$b$$")
         brace_rec = Brace(rectangle, LEFT)
         text_rec = brace_rec.get_text(r"$${h}\over{2}$$")
 
         self.draw_title(main_tri)
 
-        self.play(*[
-            Write(i) for i in (brace_b, brace_h, text_b, text_h)
+        self.my_play(*[
+            FadeIn(i, shift=j) for i, j in zip(
+                (brace_b, brace_h, text_b, text_h),
+                (UP, LEFT, UP, LEFT)
+            )
         ])
-        self.play(Create(rectangle), Write(brace_rec), Write(text_rec))
-        self.play(*[
+        self.my_play(Create(rectangle), FadeIn(brace_rec, shift=RIGHT),
+                     FadeIn(text_rec, shift=RIGHT))
+        self.my_play(*[
             FadeIn(i) for i in (KBCF, KDA, FCD)
         ], Create(line))
-        self.play(Rotate(KDA, PI, about_point=K))
+        self.my_play(Rotate(KDA, PI, about_point=K))
         FCD.generate_target()
         FCD.target.shift(LEFT * 3)
-        self.play(MoveToTarget(FCD), path_arc=210 * DEGREES, run_time=3)
+        self.my_play(MoveToTarget(FCD), path_arc=210 * DEGREES, run_time=3)
 
         self.creat_formula(r"S = {{h \times b} \over 2}")
-        self.play(FadeTransform(text_h.copy(), self.formula[0][2]),
-                  FadeTransform(text_b.copy(), self.formula[0][4]))
+        self.my_play(FadeTransform(text_h.copy(), self.formula[0][2]),
+                     FadeTransform(text_b.copy(), self.formula[0][4]))
         self.draw_formula((0, 1, 3, 5, 6))
 
 
 # Rhombus
 class Scene4(ProofScene):
     def setup(self):
-        import numpy as np
         self.create_title(r"Rhombus", r"(Hình thoi)")
 
     def construct(self):
@@ -221,32 +247,36 @@ class Scene4(ProofScene):
         CKD = VMobject(fill_color=YELLOW, **kwargs).set_points_as_corners([C, K, D, C])
 
         brace_h = Brace(main_rhombus, LEFT)
-        text_h = brace_h.get_text("h")
+        text_h = brace_h.get_text("$$h$$")
         brace_b = Brace(main_rhombus, DOWN)
-        text_b = brace_b.get_text("b")
+        text_b = brace_b.get_text("$$b$$")
         brace_h2 = Brace(CKD, RIGHT)
         h = brace_h2.get_text(r"$$h \over 2$$")
 
-        rec = Rectangle(width=5, height=1.9).align_to(main_rhombus, DR)
+        rec = Rectangle(width=5, height=1.9, color=GREEN) \
+            .align_to(main_rhombus, DR)
 
         right_angle = RightAngle(AC, BD, quadrant=(-1, 1), stroke_width=2)
 
         self.draw_title(main_rhombus)
 
-        self.play(*[
-            Write(i) for i in (brace_b, brace_h, text_b, text_h)
+        self.my_play(*[
+            FadeIn(i, shift=j) for i, j in zip(
+                (brace_b, brace_h, text_b, text_h),
+                (UP, RIGHT, UP, RIGHT)
+            )
         ])
-        self.play(*[
+        self.my_play(*[
             FadeIn(i) for i in (AKB, AKD, CKB, CKD)
         ], Create(AC), Create(BD), Create(right_angle))
 
-        self.play(AKB.animate.shift(RIGHT * 2.5 + DOWN * 1.9), run_time=2, rate_func=linear)
-        self.play(AKD.animate.shift(LEFT * 2.5 + DOWN * 1.9), run_time=2, rate_func=linear)
-        self.play(Create(rec), Write(brace_h2), Write(h))
+        self.my_play(AKB.animate.shift(RIGHT * 2.5 + DOWN * 1.9), run_time=2, rate_func=linear)
+        self.my_play(AKD.animate.shift(LEFT * 2.5 + DOWN * 1.9), run_time=2, rate_func=linear)
+        self.my_play(Create(rec), FadeIn(brace_h2, shift=LEFT), FadeIn(h, shift=LEFT))
 
         self.creat_formula(r"S = {{h \times b} \over 2}")
-        self.play(FadeTransform(text_h.copy(), self.formula[0][2]),
-                  FadeTransform(text_b.copy(), self.formula[0][4]))
+        self.my_play(FadeTransform(text_h.copy(), self.formula[0][2]),
+                     FadeTransform(text_b.copy(), self.formula[0][4]))
         self.draw_formula((0, 1, 3, 5, 6))
 
 
@@ -283,50 +313,53 @@ class Scene5(ProofScene):
         DGH = VMobject(fill_color=RED, **kwargs).set_points_as_corners([D, G, H, D])
 
         brace_h = Brace(main_rhombus, LEFT)
-        text_h = brace_h.get_text("h")
+        text_h = brace_h.get_text("$$h$$")
         brace_a = Brace(main_rhombus, DOWN)
-        text_a = brace_a.get_text("a")
+        text_a = brace_a.get_text("$$a$$")
         brace_b = Brace(Line(start=A, end=D), UP)
-        text_b = brace_b.get_text("b", buff=SMALL_BUFF)
+        text_b = brace_b.get_text("$$b$$", buff=SMALL_BUFF)
         brace_h2 = Brace(EBCH, RIGHT)
-        h = brace_h2.get_text(r"$$h \over 2$$")
+        h2 = brace_h2.get_text(r"$$h \over 2$$")
 
-        rec = Rectangle(width=6.5, height=1.5).align_to(main_rhombus, DL)
+        rec = Rectangle(width=6.5, height=1.5, color=GREEN).align_to(main_rhombus, DL)
 
         right_angle_1 = RightAngle(vertical_line1, horizontal_line2, quadrant=(-1, 1), stroke_width=2)
         right_angle_2 = RightAngle(vertical_line2, horizontal_line2, quadrant=(-1, 1), stroke_width=2)
 
         self.draw_title(main_rhombus)
 
-        self.play(*[
-            Write(i) for i in (brace_b, brace_h, brace_a, text_b, text_h, text_a)
+        self.my_play(*[
+            FadeIn(i, shift=j) for i, j in zip(
+                (text_h, brace_h, brace_a, text_a, text_b, brace_b),
+                (RIGHT, RIGHT, UP, UP, DOWN, DOWN)
+            )
         ])
-        self.play(*[
+        self.my_play(*[
             Write(i) for i in (vertical_line1, vertical_line2,
-                               horizontal_line1, h, brace_h2,
+                               horizontal_line1,
                                right_angle_1, right_angle_2)
-        ])
-        self.play(LaggedStart( *[
+        ], *[FadeIn(i, shift=LEFT) for i in (h2, brace_h2)])
+        self.my_play(LaggedStart(*[
             FadeIn(i) for i in (AEF, AFGD, DGH, EBCH)
         ], lag_ratio=0.2))
 
-        self.play(Rotate(AEF, PI, about_point=E),
-                  Rotate(DGH, -PI, about_point=H))
-        self.play(*[
+        self.my_play(Rotate(AEF, PI, about_point=E),
+                     Rotate(DGH, -PI, about_point=H))
+        self.my_play(*[
             i.animate.shift(RIGHT * j)
-            for i,j in zip(
-                (brace_b, text_b, AFGD, brace_h2, h),
-                (3,3,3,2,2)
+            for i, j in zip(
+                (brace_b, text_b, AFGD, brace_h2, h2),
+                (3, 3, 3, 2, 2)
             )
         ])
-        self.play(*[
+        self.my_play(*[
             i.animate.shift(DOWN * 1.5)
             for i in (brace_b, text_b, AFGD)
         ])
-        self.play(Create(rec))
+        self.my_play(Create(rec))
 
         self.creat_formula(r"S = {{(a+b) \times h} \over 2}")
-        self.play(FadeTransform(text_a.copy(), self.formula[0][3]),
-                  FadeTransform(text_b.copy(), self.formula[0][5]),
-                  FadeTransform(text_h.copy(), self.formula[0][8]))
-        self.draw_formula((0,1,2,4,6,7,9,10))
+        self.my_play(Transform(text_a.copy(), self.formula[0][3]),
+                     Transform(text_b.copy(), self.formula[0][5]),
+                     Transform(text_h.copy(), self.formula[0][8]))
+        self.draw_formula((0, 1, 2, 4, 6, 7, 9, 10))
--- projects/perimeter_and_area/area_of_common_shape/regular_hexagon.py
+++ projects/perimeter_and_area/area_of_common_shape/regular_hexagon.py
@@ -3,10 +3,10 @@ from manim import *
 list_scene = ("Scene1", "Scene2", "Scene3", "Scene4", "Scene5", "Scene6")
 SCENE_NAME = list_scene[0]
 CONFIG_DIR = "../../../configs/"
-CONFIG = "develop.cfg"
+CONFIG = "production.cfg"
 
 if __name__ == "__main__":
-    command = f"manim -c {CONFIG_DIR}{CONFIG} {__file__} {SCENE_NAME}"
+    command = f"manim --disable_caching -c {CONFIG_DIR}{CONFIG} {__file__} {SCENE_NAME}"
     print("cmd[" + command + "]")
     os.system(command)
 
@@ -22,22 +22,40 @@ class ProofScene(Scene):
             .next_to(self.title_en, DOWN)
 
     def draw_title(self, other_obj):
-        self.play(Write(self.title_en), Write(self.title_vn),
+        self.my_play(Write(self.title_en), Write(self.title_vn),
                   Write(other_obj), )
 
     def creat_formula(self, formula):
         self.formula = MathTex(formula).scale(1.5).shift(DOWN * 2.5)
 
     def draw_formula(self, indexs):
-        self.play(LaggedStart(*[
+        self.my_play(LaggedStart(*[
             DrawBorderThenFill(self.formula[0][i]) for i in indexs
         ], lag_ratio=0.1))
-        self.play(Circumscribe(self.formula))
+        self.my_play(Circumscribe(self.formula))
+
+    def my_play(
+            self,
+            *args,
+            subcaption=None,
+            subcaption_duration=None,
+            subcaption_offset=0,
+            **kwargs,
+    ):
+        if "run_time" not in kwargs:
+            kwargs["run_time"] = 2
+        super().play(*args,
+                     subcaption=subcaption,
+                     subcaption_duration=subcaption_duration,
+                     subcaption_offset=subcaption_offset,
+                     **kwargs)
+        self.wait()
 
 
 # regular hexagon
 class Scene1(ProofScene):
     def setup(self):
+        super().setup()
         self.create_title(r"Regular hexagon", r"(Lục giác đều)")
 
     def construct(self):
@@ -77,22 +95,31 @@ class Scene1(ProofScene):
             .next_to(text_equil_tri_vn, DOWN)
         text_remember = Text("Remember?", font_size=20).next_to(text_s, DOWN)
 
+        mark_location = [j.point_from_proportion(i) for i in (1 / 4, 3 / 4)
+                         for j in lines]
+        mark_rotation = [PI / 2, -PI / 4, PI / 4, PI / 2, -PI / 4, PI / 4]
+        marks = [MathTex("=").move_to(i).rotate(j)
+                 for i, j in zip(mark_location, mark_rotation)]
+
         arrow = Arrow(start=text_equil_tri_en.get_left(),
                       end=lines[2].point_from_proportion(0.75))
 
         self.draw_title(hexagon)
-        self.play( Write(brace_a), Write(text_a))
-        self.play(Create(circle))
-        self.play(LaggedStart(*[
+        self.my_play(*[FadeIn(i, shift=UP) for i in (brace_a, text_a)])
+        self.my_play(Create(circle))
+        self.my_play(LaggedStart(*[
             Create(line) for line in lines
         ], lag_ratio=0.2))
-        self.play(LaggedStart(*[
+        self.my_play(LaggedStart(*[
             Create(angle) for angle in angles
-        ], lag_ratio=0), Write(text_angle))
-        self.play(Write(sub_tri), *[
-            FadeOut(i) for i in (circle, *angles[0:4], angles[5])
+        ], *[
+            Create(mark) for mark in marks
+             ], lag_ratio=0), Write(text_angle))
+        self.my_play(Write(sub_tri), *[
+            FadeOut(i) for i in (circle, *angles[0:4], *marks[0:4],
+                                 angles[5])
         ])
-        self.play(*[
+        self.my_play(*[
             Write(i) for i in (arrow, text_equil_tri_en, text_equil_tri_vn,
                                text_s, text_remember)
         ])
@@ -100,15 +127,13 @@ class Scene1(ProofScene):
         #
         self.creat_formula(r"S = 6 \times {{\sqrt{3}a^2} \over 4}")
         copy_tri = sub_tri.copy().scale(0.4).move_to(self.formula[0][9])
-        self.play(ReplacementTransform(sub_tri.copy(), copy_tri), Write(self.formula[0][:4]))
-        self.play(FadeOut(copy_tri),
+        self.my_play(ReplacementTransform(sub_tri.copy(), copy_tri), Write(self.formula[0][:4]))
+        self.my_play(FadeOut(copy_tri),
                   FadeTransformPieces(text_s[0][4:].copy(), self.formula[0][4:]))
         result = MathTex(r"S = {3 \over 2}{\sqrt{3}a^2}") \
             .scale(1.5).align_to(self.formula, DL)
-        self.play(*[Transform(self.formula[0][i], result[0][j])
+        self.my_play(*[Transform(self.formula[0][i], result[0][j])
                     for i, j in zip([0, 1, 2, 4, 5, 6, 7, 8, 9, 10],
                                     [0, 1, 2, 5, 6, 7, 8, 9, 3, 4])
                     ], FadeOut(self.formula[0][3]))
-        self.play(Circumscribe(result))
-
-        self.wait()
+        self.my_play(Circumscribe(result, run_time=2))
