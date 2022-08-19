72cd9f361657d34fdeea01dc2d4f76dbdbe60020
TruongAnim <truonganim.dev@gmail.com>
2022-08-19 14:45:12 +0700

    edit project area of a circle

--- projects/perimeter_and_area/area_of_a_circle/main_scene.py
+++ projects/perimeter_and_area/area_of_a_circle/main_scene.py
@@ -1,12 +1,8 @@
-import math
-
-from manim import *
-from decimal import *
-
-getcontext().prec = 30
+from common.svg.character.number_creature import *
+from common.svg.character.number_creature_anim import *
 
 list_scene = ("Scene1", "Scene2", "Scene3", "Scene31", "Scene4", "Scene5", "Scene6")
-SCENE_NAME = list_scene[0]
+SCENE_NAME = list_scene[1]
 # SCENE_NAME = " ".join(list_scene)
 CONFIG_DIR = "../../../configs/"
 CONFIG = "develop.cfg"
@@ -19,29 +15,61 @@ if __name__ == "__main__":
 
 class Scene1(Scene):
     def construct(self):
-        h = Decimal(1000000000)
-        b = Decimal(0.02)
-        area = h*b/2
-        print(area)
+        circle = Circle(radius=2, fill_color=YELLOW,
+                        stroke_width=2,
+                        fill_opacity=0.7)
+        center = Dot(circle.get_center(), color=RED).scale(0.5)
+        area_text = MathTex("S = \pi r^2").to_corner(UR, buff=LARGE_BUFF)
+        r_line = Line(start=circle.points[0], end=circle.get_center())
+        r = MathTex("r").next_to(r_line, UP)
+        arrow = Arrow(start=area_text.get_left(), end=r.get_center() + UP * 0.5)
+        pi = NumberCreature(
+            file_name_prefix="PiCreatures",
+            mode="wonder"
+        ).to_corner(DL)
+        rectangle = Rectangle(width=2 * PI, height=2, stroke_width=1) \
+            .shift(RIGHT * 3)
+        self.play(Write(circle), Write(center))
+        self.play(Write(r_line), Write(r))
+        self.play(Write(area_text), Write(arrow))
+        self.play(FadeIn(pi, shift=RIGHT * 2))
+        bubble_kwargs = {
+            "stretch_width": 3,
+            "stretch_height": 2,
+            "stroke_width": 2,
+            "stroke_color": WHITE
+        }
+        wbw = Text("Wait... but why?")
+        self.play(NumberCreatureThinks(pi,
+                                       wbw,
+                                       target_mode="wonder",
+                                       bubble_kwargs=bubble_kwargs
+                                       )
+                  )
+
+        self.play(*[
+            FadeOut(i) for i in (area_text, arrow, pi, pi.bubble, wbw, r_line, r)
+        ])
+        self.play(*[
+            i.animate.shift(LEFT * 4) for i in (circle, center)
+        ])
+        self.play(Create(rectangle))
 
 
-        b2 = Decimal(0.01)
-        ab = Decimal(h*h+b2*b2).sqrt()
+class Scene2(Scene):
+    def construct(self):
+        circle = Circle(radius=2, fill_color=YELLOW,
+                        stroke_width=2,
+                        fill_opacity=0.7).shift(LEFT * 4)
+        center = Dot(circle.get_center(), color=RED).scale(0.5)
+        rectangle = Rectangle(width=2 * PI, height=2, stroke_width=1) \
+            .shift(RIGHT * 3)
+        self.add(circle, center, rectangle)
+        sector = Sector(
+            outer_radius=2,
+            inner_radius=0,
+            angle=40*DEGREES,
+            start_angle=0,
+        ).shift(LEFT*4)
 
-        a1 = Decimal(10000000.01)
-        a2 = Decimal(10000000.02)
-        a3 = Decimal(0.03)
-        s = (a1+a2+a3)/2
-
-        area2 = s*(s-a1)*(s-a2)*(s-a3)
-        print(area2.sqrt())
-
-        b1 = 10000.1
-        b2 = 10000.2
-        b3 = 300
-
-        s = 0.5*(b1+b2+b3)
-        print(s)
-        print(math.sqrt(s*(s-b1)*(s-b2)*(s-b3)))
-        s = 0.5 * (b1+b2+b3)
-        print(math.sqrt(s*b1*b2*b3))
+        self.play(Write(sector))
