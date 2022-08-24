db4d12d60a511fad0dcaf5e21bdfc2566778c5b8
TruongAnim <truonganim.dev@gmail.com>
2022-08-24 22:56:00 +0700

    add project how accurate is heron formula

---
+++ projects/unclassified/how_accurate_is_heron_formula/main_scene.py
@@ -0,0 +1,36 @@
+from manim import *
+
+list_scene = ("Scene1", "Scene2", "Scene3", "Scene31", "Scene4", "Scene5", "Scene6")
+SCENE_NAME = list_scene[0]
+# SCENE_NAME = " ".join(list_scene)
+CONFIG_DIR = "../../../configs/"
+CONFIG = "production.cfg"
+
+if __name__ == "__main__":
+    command = f"manim -c {CONFIG_DIR}{CONFIG} {__file__} {SCENE_NAME}"
+    print("cmd[" + command + "]")
+    os.system(command)
+
+
+class MyScene(Scene):
+    def my_play(
+            self,
+            *args,
+            subcaption=None,
+            subcaption_duration=None,
+            subcaption_offset=0,
+            **kwargs,
+    ):
+        if "run_time" not in kwargs:
+            kwargs["run_time"] = 2
+        super().play(*args,
+                     subcaption=subcaption,
+                     subcaption_duration=subcaption_duration,
+                     subcaption_offset=subcaption_offset,
+                     **kwargs)
+        self.wait()
+
+
+class Scene1(MyScene):
+    def construct(self):
+        self.play(Write(Square()))
