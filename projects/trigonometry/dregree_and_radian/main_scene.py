0dc105be2d878acc9f279e8185259f90c5bfda1c
TruongAnim <truonganim.dev@gmail.com>
2022-09-08 22:40:43 +0700

    add projects degree_and_radian

---
+++ projects/trigonometry/dregree_and_radian/main_scene.py
@@ -0,0 +1,31 @@
+from manim import *
+
+list_scene = ("Scene1", "Scene2", "Scene3", "Scene4", "Scene5", "Scene6")
+SCENE_NAME = list_scene[4]
+# SCENE_NAME = " ".join(list_scene)
+CONFIG_DIR = "../../../configs/"
+CONFIG = "production.cfg"
+
+if __name__ == "__main__":
+    command = f"manim -c {CONFIG_DIR}{CONFIG} {__file__} {SCENE_NAME}"
+    print("cmd[" + command + "]")
+    os.system(command)
+
+
+class MyScene(Scene):
+    def my_play(
+            self,
+            *args,
+            subcaption=None,
+            subcaption_duration=None,
+            subcaption_offset=0,
+            **kwargs,
+    ):
+        if "run_time" not in kwargs:
+            kwargs["run_time"] = 2
+        super().play(*args,
+                     subcaption=subcaption,
+                     subcaption_duration=subcaption_duration,
+                     subcaption_offset=subcaption_offset,
+                     **kwargs)
+        self.wait()
