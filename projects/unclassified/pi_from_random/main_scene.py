4560b19f33c48bc379b1b748e790be8aaf86f3d1
TruongAnim <truonganim.dev@gmail.com>
2022-11-01 17:44:06 +0700

    edit project pi from random

--- projects/unclassified/pi_from_random/main_scene.py
+++ projects/unclassified/pi_from_random/main_scene.py
@@ -1,12 +1,14 @@
-from common.svg.character.number_creature import *
-from common.svg.character.number_creature_anim import *
+import math
+
+from manim import *
+import random
 
 list_scene = ("Scene0", "Scene1", "Scene2", "Scene3", "Scene4", "Scene5",
               "Scene6", "Scene7", "Scene8", "Scene9")
-SCENE_NAME = list_scene[2]
+SCENE_NAME = list_scene[4]
 # SCENE_NAME = " ".join(list_scene)
 CONFIG_DIR = "../../../configs/"
-CONFIG = "develop.cfg"
+CONFIG = "production.cfg"
 
 if __name__ == "__main__":
     command = f"manim -c {CONFIG_DIR}{CONFIG} {__file__} {SCENE_NAME}"
@@ -155,3 +157,160 @@ class Scene2(MyScene):
         self.my_play(Write(formula5))
         self.my_play(Circumscribe(formula5[-1]))
 
+
+class Scene3(MyScene):
+    def construct(self):
+        import random
+        main_circle = Circle(radius=3, color=BLUE)
+        vertical = Arrow(start=DOWN * 4, end=UP * 4, stroke_width=1)
+        horizontal = Arrow(start=LEFT * 4, end=RIGHT * 4, stroke_width=1)
+        O = Dot(ORIGIN)
+        a = Dot(RIGHT * 2.2 + UP * 1.3, color=YELLOW)
+        a_t = MathTex("A", color=YELLOW).scale(0.7).next_to(a, RIGHT, buff=SMALL_BUFF)
+        b = Dot(RIGHT * 1.9 + UP * 2.6, color=RED)
+        b_t = MathTex("B", color=RED).scale(0.7).next_to(b, RIGHT, buff=SMALL_BUFF)
+        A = VGroup(a, a_t)
+        B = VGroup(b, b_t)
+        sub_circle = main_circle.get_subcurve(0, 0.25)
+        sub_circle.add_points_as_corners([ORIGIN, RIGHT*3])
+        sub_circle.set_stroke(color=YELLOW, width=2)
+        sub_circle.set_fill(color=YELLOW, opacity=0.2)
+        square = VMobject(stroke_width=2, stroke_color=WHITE, fill_color=WHITE, fill_opacity=0.2)\
+            .set_points_as_corners([ORIGIN, RIGHT*3, UR*3, UP*3, ORIGIN])
+        O_t = MathTex("O").next_to(O, DL)
+        x_t = MathTex("x").next_to(horizontal.get_end(), DOWN)
+        y_t = MathTex("y").next_to(vertical.get_end(), RIGHT)
+        brace = Brace(Line(start=ORIGIN, end=RIGHT * 3), DOWN, buff=SMALL_BUFF, color=GREEN)
+        brace_t = brace.get_tex("1")
+        brace_t.set_color(GREEN)
+        brace2 = Brace(Line(start=ORIGIN, end=UP * 3), LEFT, buff=SMALL_BUFF, color=GREEN)
+        brace2_t = brace2.get_tex("1")
+        brace2_t.set_color(GREEN)
+        group = VGroup(main_circle, vertical, horizontal, O, O_t, x_t, y_t, brace, brace2, brace_t, brace2_t, A, B,
+                       sub_circle, square).shift(LEFT * 3.5)
+        A_t = MathTex("A(0.73, 0.43)", color=YELLOW).shift(RIGHT*2+UP*2)
+        OA = Line(O.get_center(), a.get_center(), color=YELLOW)
+        OA_t = MathTex("OA = \sqrt{0.73^2+0.43^2}=", "0.85", color=YELLOW).next_to(A_t, DOWN, aligned_edge=LEFT)
+        B_t = MathTex("B(0.63, 0.86)", color=RED).next_to(OA_t, DOWN, aligned_edge=LEFT, buff=LARGE_BUFF)
+        OB = Line(O.get_center(), b.get_center(), color=RED)
+        OB_t = MathTex("OB = \sqrt{0.63^2+0.86^2}=", "1.07", color=RED).next_to(B_t, DOWN, aligned_edge=LEFT)
+        self.play(Create(horizontal), Create(vertical), Write(O_t), Create(O), Write(x_t), Write(y_t))
+        self.play(Create(main_circle), FadeIn(brace, shift=UP), FadeIn(brace_t, shift=UP),
+                     FadeIn(brace2_t, shift=RIGHT), FadeIn(brace2, shift=RIGHT))
+        self.my_play(Write(A_t), Write(B_t))
+        self.my_play(LaggedStart(Transform(A_t.copy(), A), Transform(B_t.copy(), B), lag_ratio=0.5))
+        self.my_play(Create(OA), Write(OA_t))
+        self.my_play(Create(OB), Write(OB_t))
+        self.my_play(Wiggle(OA_t[-1], scale_value=1.5))
+        self.my_play(Wiggle(OB_t[-1], scale_value=1.5))
+        # self.add(group, OA, OA_t, A_t, B_t, OB_t, OB)
+
+
+rel_obj = 500000
+rel_time = 100
+test_obj = 100
+test_time = 3
+
+
+class Scene4(MyScene):
+    def get_random_position(self):
+        x = random.uniform(0, 5)
+        y = random.uniform(0, 5)
+        return np.array([x, y, 0])
+
+    def construct(self):
+        random.seed(1)
+        shape = Circle(radius=5, stroke_width=3, stroke_color=YELLOW).shift(DOWN*2.5).get_subcurve(0, 0.25)
+        square = Square(side_length=5, stroke_color=WHITE, stroke_width=3).shift(RIGHT*2.5)
+        brace1 = Brace(square, DOWN)
+        side1 = brace1.get_tex("1")
+        brace2 = Brace(square, LEFT)
+        side2 = brace2.get_tex("1")
+
+        vertical = Arrow(start=square.point_from_proportion(0.5)+LEFT, end=square.point_from_proportion(0.75)+RIGHT, stroke_width=1)
+        horizontal = Arrow(start=square.point_from_proportion(0.5)+DOWN, end=square.point_from_proportion(0.25)+UP, stroke_width=1)
+
+        self.my_play(Create(vertical), Create(horizontal))
+        self.my_play(DrawBorderThenFill(shape))
+        self.my_play(Write(square))
+        self.my_play(FadeIn(brace1, shift=UP), FadeIn(side1, shift=UP), FadeIn(brace2, shift=RIGHT), FadeIn(side2, shift=RIGHT))
+
+        formula = MathTex(
+            r"\pi",  # 0
+            r"\approx",  # 1
+            r"{\text{aaaaaaaa}",  # 2
+            r"\over",  # 3
+            r"\text{aaaaaaaa}}",  # 4
+            r"\times",  # 5
+            r"4",  # 6
+        ).scale(1.2).shift(LEFT * 4.7)
+        for i in (2, 4):
+            formula[i].set_color(BLACK)
+
+        formula[0].set_color(RED)
+        formula2 = formula[1].copy().shift(DOWN*1.5)
+
+        real_pi = Text("{:.10f}...".format(math.pi),
+                     color=GREEN,
+                     font_size=35,
+                     font="Arial") \
+                .next_to(formula2, RIGHT, aligned_edge=LEFT, buff=MED_LARGE_BUFF)\
+            .shift(DOWN)
+
+        self.my_play(LaggedStart(*[
+            Write(formula),
+            Write(formula2),
+            Write(real_pi)
+        ], lag_ratio=0.3))
+
+        self.num_dot = 0
+        self.num_green = 0
+
+        tracker = ValueTracker(0)
+
+        def draw_tex():
+            dot_green = Text(str(self.num_green),
+                             color=YELLOW,
+                             font_size=35,
+                             font="Arial") \
+                .move_to(formula[2])
+            dot_total = Text(str(self.num_dot),
+                             color=WHITE,
+                             font_size=35,
+                             font="Arial") \
+                .move_to(formula[4])
+            result = "0.0000"
+            if self.num_dot != 0:
+                result = "{:.5f}".format(4 * self.num_green / self.num_dot)
+            s = Text(result,
+                     color=RED,
+                     font_size=35,
+                     font="Arial") \
+                .next_to(formula2, RIGHT, aligned_edge=LEFT, buff=MED_LARGE_BUFF)
+            return VGroup(dot_green, dot_total, s)
+
+        group = always_redraw(draw_tex)
+
+        self.add(tracker, group)
+        def update(obj):
+            value = tracker.get_value()
+            if int(value) > self.num_dot:
+                more_point = int(value) - self.num_dot
+                self.num_dot = int(value)
+                for i in range(more_point):
+                    p = self.get_random_position()
+                    # dot = Square(side_length=0.01, stroke_width=2).move_to(p+DOWN*2.5)
+                    if p[0]*p[0]+p[1]*p[1]<25:
+                        # dot.set_color(YELLOW)
+                        self.num_green += 1
+                    else:
+                        # dot.set_color(WHITE)
+                        pass
+                    # self.add(dot)
+
+        shape.add_updater(update)
+
+        self.my_play(tracker.animate.increment_value(rel_obj),
+                  run_time=rel_time,
+                  rate_func=linear)
+        self.play(Circumscribe(group[2]))
