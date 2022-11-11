3a65187fa4beb4648e5cbedad535dd04354b70ec
TruongAnim <truonganim.dev@gmail.com>
2022-11-11 23:04:54 +0700

    edit project euclid_theorem_proof

--- projects/special_number/euclid_theorem_proof/count_prime.py
+++ projects/special_number/euclid_theorem_proof/count_prime.py
@@ -1,15 +1,18 @@
-lower_value = 10000
-upper_value = 100000
+lower_value = 2
+upper_value = 1000
 
 print("The Prime Numbers in the range are: ")
 count = 0
+l = []
 for number in range(lower_value, upper_value + 1):
-    for i in range(2, number // 2):
+    for i in range(2, (number // 2)+1):
         if (number % i) == 0:
             # print(n,"is not a prime number")
             break
     else:
         count += 1
-    if number%lower_value == 0:
-        print(count)
+        l.append(number)
+    # if number%lower_value == 0:
+    #     print(count)
 print(count)
+print(l)
