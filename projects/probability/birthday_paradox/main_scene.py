805b600dd43e1c4b316e18a3da0eee6d44f72047
TruongAnim <truonganim.dev@gmail.com>
2022-11-20 08:35:41 +0700

    test avatar set

--- projects/probability/birthday_paradox/main_scene.py
+++ projects/probability/birthday_paradox/main_scene.py
@@ -3,10 +3,10 @@ from common.custom.custom_mobject import TextTranslation
 from common.custom.custom_mobject import Explain
 
 list_scene = ("Scene0", "Scene1", "Scene2", "Scene3")
-SCENE_NAME = list_scene[1]
+SCENE_NAME = list_scene[0]
 # SCENE_NAME = " ".join(list_scene)
 CONFIG_DIR = "../../../configs/"
-CONFIG = "production.cfg"
+CONFIG = "develop.cfg"
 
 if __name__ == "__main__":
     command = f"manim -c {CONFIG_DIR}{CONFIG} {__file__} {SCENE_NAME}"
@@ -39,4 +39,6 @@ myTemplate.add_to_preamble(r"\usepackage{vntex}")
 
 class Scene0(MyScene):
     def construct(self):
-        pass
+        names = ["chars/char (" + str(i) + ")" for i in range(1, 11)]
+        group = Group(*[ImageMobject(i).scale_to_fit_height(1.7) for i in names]).arrange_in_grid(5, 5, buff=-0.2)
+        self.add(group)
